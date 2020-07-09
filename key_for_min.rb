6f9ddc082773e0089e9d930520310a5d9b071654
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 19:42:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,3 +18,9 @@
 # end
 
 
+def key_for_min_value(name_hash)
+
+  values_array = name_hash.collect{|key, value| value}.sort
+ name_hash.each{|key, value| return key if value == values_array[0]}
+ nil
+end

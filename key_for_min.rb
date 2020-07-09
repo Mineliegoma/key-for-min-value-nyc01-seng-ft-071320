8d6965fa08a613a1fb2248cc19d889b74cbcf328
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 19:30:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,12 @@
 def key_for_min_value(name_hash)
 value_of_array=name_hash.collect{|key,value|value}.sort 
 #Think about how to determine which value is the lowest. Do you need to compare each value to something as you iterate?
-name_hash.each {do |key,value|
+name_hash.each do |key,value|
 if value==value_of_array[0]
   return key
-  
-}
-nil
-#end
+else
+  nil
+end
 
 end
 end

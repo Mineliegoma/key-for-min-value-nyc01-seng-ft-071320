2254ac6864aafa95ab8f9729145402f9e1c1672f
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 19:25:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-value_of_array=name_hash.collect{|key,value|value}.sort #
+value_of_array=name_hash.collect{|key,value|value}.sort 
+#Think about how to determine which value is the lowest. Do you need to compare each value to something as you iterate?
+name_hash.each{|key,value|
+if value==value_array[0]
+  return key
+}
 end
